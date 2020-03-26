3a99d1a0151de8a0653ef422639dcc761277b0cb
Pascal Martin <pascal.fb.martin@gmail.com>
2020-03-25 18:55:13 -0700

    Support both PC and Raspberry Pi hardware, change style to mimic Cockpit graphs

--- temperature.js
+++ temperature.js
@@ -1,31 +1,60 @@
 window.onload = function () {
     resize_canvas()
 
-    var chart = new SmoothieChart({millisPerPixel: 200, maxValueScale: 1.1, minValueScale: 1.1, scaleSmoothing: 0.1, grid: { fillStyle: '#ffffff', millisPerLine: 5000, verticalSections: 8 }, labels: { fillStyle: '#0000ff', fontSize: 18, precision: 1 }, timestampFormatter: SmoothieChart.timeFormatter }),
+    var chart = new SmoothieChart({tooltip:true, millisPerPixel: 500, maxValue: 100, minValue: 0, scaleSmoothing: 0.1, grid: { fillStyle: '#ffffff', millisPerLine: 0, verticalSections: 4, sharpLines: true}, labels: { fillStyle: '#000000', fontSize: 12, precision: 0 }, timestampFormatter: SmoothieChart.timeFormatter }),
         canvas = document.getElementById('smoothie-chart'),
-        series = new TimeSeries();
-        average_array = []
+        series = [];
+        stokes = [{lineWidth: 3.5, strokeStyle: '#ff0000',fillStyle:'rgba(255,0,0,0.08)'},
+                  {lineWidth: 3.5, strokeStyle:  '#008800',fillStyle:'rgba(0,136,0,0.08)'},
+                  {lineWidth: 3.5, strokeStyle:  '#0000ff',fillStyle:'rgba(0,0,255,0.08)'}];
 
-    chart.addTimeSeries(series, { lineWidth: 3.5, strokeStyle: '#ff0000' });
-    chart.streamTo(canvas, 500);
-
-    function run_proc(series,average_array) {
-        var proc = cockpit.spawn(["vcgencmd","measure_temp"]);
+    function run_init () {
+        var i = 0;
+        var proc = cockpit.spawn(["sensors"]);
         proc.done(function(data){
-            pt = parseFloat(data.match(/([0-9\.]+)/)[1]);
-            series.append(new Date().getTime(), pt);
-            document.getElementById("cur_temp").innerHTML = pt;
-            average_array.push(pt);
-            if(average_array.length > 5*60){
-                average_array.shift();
+            pts = data.match(/temp1:[\t ]+[+]*[0-9\.]+/g);
+            if (pts) {
+               for (i = 0; i < pts.length; i++) {
+                  series[i] = new TimeSeries();
+                  chart.addTimeSeries(series[i], stokes[i%stokes.length]);
+               }
             }
-            document.getElementById("avg_temp").innerHTML = average(average_array);
-            console.log(average_array);
         });
+        var proc = cockpit.spawn(["vcgencmd", "measure_temp"]);
+        proc.done(function(data){
+	        pt = parseFloat(data.match(/temp[=]([0-9\.]+)/)[1]);
+            if (pt) {
+	            series[i] = new TimeSeries();
+                chart.addTimeSeries(series[i], stokes[i%stokes.length]);
+            }
+	    });
+    };
+    run_init();
+    chart.streamTo(canvas, 5000);
+
+    function run_proc(series) {
+        var i = 0;
+        var proc = cockpit.spawn(["sensors"]);
+        proc.done(function(data){
+            pts = data.match(/temp1:[\t ]+[+]*[0-9\.]+/g);
+            if (pts) {
+               for (i = 0; i < pts.length; i++) {
+                  pt = parseFloat(pts[i].match(/temp1:[\t ]+[+]*([0-9\.]+)/)[1]);
+                  series[i].append(new Date().getTime(), pt);
+               }
+            }
+        });
+        var proc = cockpit.spawn(["vcgencmd", "measure_temp"]);
+        proc.done(function(data){
+	        pt = parseFloat(data.match(/temp[=]([0-9\.]+)/)[1]);
+            if (pt) {
+                series[i].append(new Date().getTime(), pt);
+            }
+	    });
     };
 
     //run_proc(series)
-    setInterval(function () { run_proc(series,average_array) }, 1000);
+    setInterval(function () { run_proc(series) }, 5000);
 }
 
 function resize_canvas() {
